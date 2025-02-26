b5157d9aafe5532295bb70977cb2548137c12bbb
shivamsaksham <shivamsaksham2000@gmail.com>
2025-02-26 14:19:20 +0530

    fix: changfed name of icons with sapce in name

--- src/iconse.tsx
+++ src/iconse.tsx
@@ -1903,15 +1903,15 @@ export { default as Zoom } from './icons/zoom.svg';
 
 export { default as DoubleBlueTick } from './icons/blue-tick.svg';
 
-export { default as VoiceNote } from './icons/voice note.svg';
+export { default as VoiceNote } from './icons/voicenote.svg';
 
-export { default as SingleTick } from './icons/single tick.svg';
+export { default as SingleTick } from './icons/singletick.svg';
 
 export { default as Pending } from './icons/pending.svg';
 
 export { default as Image2 } from './icons/image2.svg';
 
-export { default as DoubleTick } from './icons/double tick.svg';
+export { default as DoubleTick } from './icons/doubletick.svg';
 
 export { default as Doc } from './icons/doc.svg';
 
